442237f7a64caadd758a9da14094a74dd5bc700f
Administrator <administrator@SYM0474.local>
2022-06-10 15:49:06 +0200

    dodatpything

---
+++ test.py
@@ -0,0 +1,31 @@
+import boto3
+
+ses = boto3.client('ses')
+
+body = """
+    Hello, this is a test for a python script for Amazon SES
+
+    Aleksa
+"""
+
+
+ses.send_email(
+    Source = 'pleskovicaleksa99@gmail.com',
+    Destination={
+        'ToAddresses': [
+            "stevan.kosijer@symphony.is",
+        ]
+    }
+    Message={
+        'Subject': {
+            'Data': 'SES Demo',
+            'Charset': 'UTF-8'
+        },
+        'Body': {
+            'Text': {
+                'Data' : body,
+                'Charset': 'UTF-8'
+            }
+        }
+    }
+)
